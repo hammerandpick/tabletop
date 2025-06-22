b9dbf980cf60f9a24edc4f3add6087191a38ef17
hammerandpick <hammerandpick@nyware.de>
2025-06-22 15:12:01 +0200

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'myPWA-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/favicon.ico',
+    '/styles.css',
+    '/main.js'
+];
+
+// Install event: cache files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+// Activate event: clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+// Fetch event: serve cached content when offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
